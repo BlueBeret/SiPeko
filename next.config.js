610e70ea21e8a28581ed2f820836b594bb098350
blueberet <63086194+Blueberet@users.noreply.github.com>
2022-09-19 23:13:15 +0700

    dep: add axios

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  rewrites: () => { return [{ source: '/api/:path*', destination: process.env.BACKEND_URL + '/:path*' }] }
 }
 
 module.exports = nextConfig
