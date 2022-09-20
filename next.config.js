192027709b0ae08ea578061bd81d9e28f1a8ab09
blueberet <63086194+Blueberet@users.noreply.github.com>
2022-09-20 11:04:01 +0700

    fix: allow localhost to host image

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['localhost'],
+  },
   rewrites: () => { return [{ source: '/api/:path*', destination: process.env.BACKEND_URL + '/:path*' }] }
 }
 
